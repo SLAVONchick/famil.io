f90c3af2c53e28bc68737f46dcc61d32a3cf56bb
SLAVONchick <viacheslavkoryagin@gmail.com>
2019-06-28 13:14:23 +0300

    little refactoring

--- src/Server/Db.fs
+++ src/Server/Db.fs
@@ -1,40 +1,41 @@
-namespace Server.Db
-
-open LinqToDB.Identity
-open LinqToDB
-open LinqToDB.Configuration
-open Server.Tables
-
+namespace Server
 module Db =
-    [<Literal>]
-    let dbName = "familio"
 
-type ConnectionStringSettings(connStr, isGlobal, name, provider) =
-    member val ConnectionString = connStr with get, set
-    member val IsGlobal = isGlobal with get, set
-    member val Name = name with get, set
-    member val ProviderName = provider with get, set
-    new(connString, name) =
-        ConnectionStringSettings(connString, true, name, LinqToDB.ProviderName.PostgreSQL)
-    interface IConnectionStringSettings with
-        member x.ConnectionString = x.ConnectionString
-        member x.IsGlobal = x.IsGlobal
-        member x.Name = x.Name
-        member x.ProviderName = x.ProviderName
-
-
-type DbSettings(providers, conf, defaultProvider, connStrings) =
-    interface ILinqToDBSettings with
-        member x.DataProviders = providers
-        member x.DefaultConfiguration = conf
-        member x.DefaultDataProvider = defaultProvider
-        member x.ConnectionStrings = connStrings
-
-type DbFamilio() =
-    inherit LinqToDB.Data.DataConnection(Db.dbName)
-
-    member x.Roles with get() = x.GetTable<Role>()
-    member x.Groups with get() = x.GetTable<Group>()
-    member x.UsersRolesGroups with get() = x.GetTable<UsersRolesGroups>()
-    member x.Tasks with get() = x.GetTable<Task>()
-    member x.Comments with get() = x.GetTable<Comment>()
+    open LinqToDB.Identity
+    open LinqToDB
+    open LinqToDB.Configuration
+    open Server.Tables
+    
+    module Db =
+        [<Literal>]
+        let dbName = "familio"
+    
+    type ConnectionStringSettings(connStr, isGlobal, name, provider) =
+        member val ConnectionString = connStr with get, set
+        member val IsGlobal = isGlobal with get, set
+        member val Name = name with get, set
+        member val ProviderName = provider with get, set
+        new(connString, name) =
+            ConnectionStringSettings(connString, true, name, LinqToDB.ProviderName.PostgreSQL)
+        interface IConnectionStringSettings with
+            member x.ConnectionString = x.ConnectionString
+            member x.IsGlobal = x.IsGlobal
+            member x.Name = x.Name
+            member x.ProviderName = x.ProviderName
+    
+    
+    type DbSettings(providers, conf, defaultProvider, connStrings) =
+        interface ILinqToDBSettings with
+            member x.DataProviders = providers
+            member x.DefaultConfiguration = conf
+            member x.DefaultDataProvider = defaultProvider
+            member x.ConnectionStrings = connStrings
+    
+    type DbFamilio() =
+        inherit LinqToDB.Data.DataConnection(Db.dbName)
+    
+        member x.Roles with get() = x.GetTable<Role>()
+        member x.Groups with get() = x.GetTable<Group>()
+        member x.UsersRolesGroups with get() = x.GetTable<UsersRolesGroups>()
+        member x.Tasks with get() = x.GetTable<Task>()
+        member x.Comments with get() = x.GetTable<Comment>()
--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -1,11 +1,19 @@
 namespace Shared
 open System
 
-type Counter = { Value : int }
+module Functions =
+    let inline (|IsBigEnough|_|) len (input:^a) =
+        let inputLen = (^a: (member Length : int) input)
+        if inputLen >= len then Some () else None
+
+    let prepare (nick: string) =
+        match nick with
+        | IsBigEnough 3 -> "*" + nick + "*"
+        | _ -> nick + "*"
 
 type Status = 
-  | Created = 1
-  | Closed = 2
+    | Created = 1
+    | Closed = 2
 
 type Priority = Highest = 1
 
@@ -19,38 +27,38 @@ type Roles =
     | Admin = 2
 
 module Dto =
-  type RoleDto =
-      { Id:int
-        Name:string }
-  type GroupDto =
-      { Id: int64
-        Name: string
-        CreatedBy: string
-        CreatedAt: DateTime
-        DeletedBy: string option
-        DeletedAt: DateTime option }
-  type UsersRolesGroupsDto =
-      { UserId: string
-        RoleId: int
-        GroupId: int64 }
-  type TaskDto =
-      { Id:Guid
-        GroupId: int64
-        Name: string
-        Description: string option
-        CreatedBy: string
-        CreatedAt: DateTime
-        Executor: string
-        ExpiresBy: DateTime option
-        Status: int
-        Priority: int }
-  type CommentDto =
-      { Id: Guid
-        TaskId: Guid
-        UserId: string
-        Contents: string
-        CreatedAt: DateTime
-        UpdatedAt: DateTime option }
+    type RoleDto =
+        { Id:int
+          Name:string }
+    type GroupDto =
+        { Id: int64
+          Name: string
+          CreatedBy: string
+          CreatedAt: DateTime
+          DeletedBy: string option
+          DeletedAt: DateTime option }
+    type UsersRolesGroupsDto =
+        { UserId: string
+          RoleId: int
+          GroupId: int64 }
+    type TaskDto =
+        { Id:Guid
+          GroupId: int64
+          Name: string
+          Description: string option
+          CreatedBy: string
+          CreatedAt: DateTime
+          Executor: string
+          ExpiresBy: DateTime option
+          Status: int
+          Priority: int }
+    type CommentDto =
+        { Id: Guid
+          TaskId: Guid
+          UserId: string
+          Contents: string
+          CreatedAt: DateTime
+          UpdatedAt: DateTime option }
 
 module Option =
     let fromString =
